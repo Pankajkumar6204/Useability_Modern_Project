bab7faa4cf859041fdf23f52f471a48e57957a1f
PankajSahi <pankajkumar01072003nwd@gmail.com>
2024-03-14 11:24:47 +0530

    Add new Projects

---
+++ script.js
@@ -0,0 +1,7 @@
+// Nav-links toggle
+let openLinks = document.getElementById('nav-toggle')
+let navLinks = document.getElementById('nav-links')
+openLinks.addEventListener('click', () =>{
+    navLinks.classList.toggle('active')
+
+})
